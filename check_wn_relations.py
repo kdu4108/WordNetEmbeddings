7a21aaae2222a472abcd0fe8539a1f72a7debef8
Kevin Du <kevin.du20@gmail.com>
2023-06-22 10:56:44 +0200

    Bugfix check_wn_relations

--- check_wn_relations.py
+++ check_wn_relations.py
@@ -6,7 +6,8 @@ ids = set([f"{p['id']}:{p['version']}" for p in wn.projects()])
 lang_to_relations: Dict[str, List[str]] = {id: [] for id in ids}
 for wn_id in ids:
     try:
-        wnet = wn.download(wn_id)
+        wn.download(wn_id)
+        wnet = wn.Wordnet(lexicon=id)
         fcts = {
             "hypernyms": (lambda x: x.hypernyms()),
             "hyponyms": (lambda x: x.hyponyms()),
@@ -18,8 +19,8 @@ for wn_id in ids:
                 if f(synset):
                     lang_to_relations[wn_id].append(f_name)
                     break
-    except (ConnectionError, urllib3.exceptions.ReadTimeoutError, TimeoutError):
+    except (ConnectionError, urllib3.exceptions.ReadTimeoutError, TimeoutError) as e:
         print(f"Unable to download wordnet {wn_id}.")
-        lang_to_relations[wn_id].append("UNABLE TO DOWNLOAD")
+        lang_to_relations[wn_id].append(f"UNABLE TO DOWNLOAD because of error {e}.")
 
 print(lang_to_relations)
