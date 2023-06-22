29558d9ab800407212c6e77363ad0776591eccbf
Kevin Du <kevin.du20@gmail.com>
2023-06-22 10:52:02 +0200

    Add check_wn_relations script to understand which languages have relations like hypernymy

---
+++ check_wn_relations.py
@@ -0,0 +1,25 @@
+from typing import Dict, List
+import wn
+import urllib3
+
+ids = set([f"{p['id']}:{p['version']}" for p in wn.projects()])
+lang_to_relations: Dict[str, List[str]] = {id: [] for id in ids}
+for wn_id in ids:
+    try:
+        wnet = wn.download(wn_id)
+        fcts = {
+            "hypernyms": (lambda x: x.hypernyms()),
+            "hyponyms": (lambda x: x.hyponyms()),
+            "meronyms": (lambda x: x.meronyms()),
+            "holonyms": (lambda x: x.holonyms()),
+        }
+        for f_name, f in fcts.items():
+            for synset in wnet.synsets():
+                if f(synset):
+                    lang_to_relations[wn_id].append(f_name)
+                    break
+    except (ConnectionError, urllib3.exceptions.ReadTimeoutError, TimeoutError):
+        print(f"Unable to download wordnet {wn_id}.")
+        lang_to_relations[wn_id].append("UNABLE TO DOWNLOAD")
+
+print(lang_to_relations)
